f79d2cc92f7e58509e9050ab48729b3fea3dfec7
S34N18 <lemabombaa@gmail.com>
2025-04-08 19:36:53 +0300

    Add frontend as a regular folder

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the homepage of your assignment management system.</p>
+    </div>
+  );
+}
+
+export default Home;
